c9287ff0ce0b770f7db7071ff22e3e0e85c82cf0
Shahrukh Malik <shahrukhm92@gmai.com>
2021-10-08 11:34:31 -0500

    local project pushed to GitHub

--- app/src/main/java/com/srmstudios/stopwatchtimer/ui/MainActivity.kt
+++ app/src/main/java/com/srmstudios/stopwatchtimer/ui/MainActivity.kt
@@ -2,6 +2,11 @@ package com.srmstudios.stopwatchtimer.ui
 
 import android.os.Bundle
 import android.view.LayoutInflater
+import androidx.navigation.NavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.setupActionBarWithNavController
+import com.srmstudios.stopwatchtimer.R
+import com.srmstudios.stopwatchtimer.databinding.ActivityMainBinding
 import com.srmstudios.stopwatchtimer.util.BaseActivity
 
 class MainActivity : BaseActivity<ActivityMainBinding>() {
@@ -9,7 +14,14 @@ class MainActivity : BaseActivity<ActivityMainBinding>() {
     override val bindingInflater: (LayoutInflater) -> ActivityMainBinding
             = ActivityMainBinding::inflate
 
+    private lateinit var navController: NavController
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        navController = (supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment)
+            .navController
+
+        setupActionBarWithNavController(navController)
     }
 }
